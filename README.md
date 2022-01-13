3736a02f5847ce5aadb5a4effacf50f584583f39
Oleksii Proshchenko <80070761+MasterpieceElbow@users.noreply.github.com>
2022-01-13 14:50:05 +0200

    Add py-distance-class github task

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-# Python boilerplate for GitHub tasks
+# Distance class
 
 - Read [the guideline](https://github.com/mate-academy/py-task-guideline/blob/main/README.md) before start
-- Implement the task described [here](app/main.py)
+
+Every day you have morning running. You want to store your result. 
+To easy calculating, comparison and printing you result write class
+`Distance`.
+
+`Distance`'s constructor takes only
+one argument `km` and saves it to `self.km`.
+
+For `Distance` class you should implement such magic
+methods:
+```python
+__init__
+distance = Distance(20) # distance.km == 20
+__str__
+print(distance) # "Distance: 20 kilometers."
+__add__
+dist2 = distance + 10 # dist2 == 30
+__iadd__
+distance += 30 # distance.km == 50
+__mul__
+dist2 = distance * 5 # dist2 == 250
+__truediv__
+dist2 = distance / 7 # dist2 == 7.14
+# Note: rounded to 2 decimals
+__mod__
+dist2 = distance % 20 # dist2 == 10
+__lt__, __gt__, __eq__, __le__, __ge__
+distance >= 50 # True
+distance == 100 # False
+distance < 60 # True
+distance <= 49 # False
+distance < 120 # True
+__len__
+len(distance) == 50
+```
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,231 @@
-# TODO: add tests
-from app.main import hello_world
+import io
+import pytest
+
+from contextlib import redirect_stdout
+
+from app.main import Distance
 
 
-def test_hello_world():
-    assert hello_world() == "Hello, world!"
+@pytest.mark.parametrize(
+    'kilometers',
+    [
+        (50,),
+        (100,),
+        (300,)
+    ]
+)
+def test_distance_class_init(kilometers):
+    distance = Distance(kilometers)
+    assert distance.km == kilometers, (
+        f"Instance attribute 'km' should equal to {kilometers} "
+        f"when you create instance with 'Distance({kilometers})'"
+    )
+
+
+@pytest.mark.parametrize(
+    'kilometers,output',
+    [
+        (50, "Distance: 50 kilometers.\n"),
+        (100, "Distance: 100 kilometers.\n"),
+        (300, "Distance: 300 kilometers.\n")
+    ]
+)
+def test_distance_class_str(kilometers, output):
+    distance = Distance(kilometers)
+    f = io.StringIO()
+
+    with redirect_stdout(f):
+        print(distance)
+
+    out = f.getvalue()
+
+    assert out == output, (
+        f"Output should equal to {output} "
+        f"when you call 'print' with instance of Distance class"
+    )
+
+
+@pytest.mark.parametrize(
+    'kilometers,kilometers2,result',
+    [
+        (50, 15, 65),
+        (100, 30, 130),
+        (300, 100, 400)
+    ]
+)
+def test_distance_class_add(kilometers, kilometers2, result):
+    distance = Distance(kilometers)
+    dist2 = distance + kilometers2
+    assert dist2 == result, (
+        f"The sum of 'Distance({kilometers})' and {kilometers2} "
+        f"should equal to {result}"
+    )
+
+
+@pytest.mark.parametrize(
+    'kilometers,kilometers2,result',
+    [
+        (50, 15, 65),
+        (100, 30, 130),
+        (300, 100, 400)
+    ]
+)
+def test_distance_class_iadd(kilometers, kilometers2, result):
+    distance = Distance(kilometers)
+    instance_1 = distance
+    distance += kilometers2
+    instance_2 = distance
+    assert distance.km == result, (
+        f"Attribute 'km' of Distance instance should become {result} "
+        f"when your initial 'km' equals to {kilometers} "
+        f"and you '+=' {kilometers2} to your instance"
+    )
+    assert instance_1 is instance_2, (
+        "__iadd__ should return the same instance"
+    )
+
+
+@pytest.mark.parametrize(
+    'kilometers,number,result',
+    [
+        (50, 3, 150),
+        (30, 7, 210),
+        (45, 5, 225)
+    ]
+)
+def test_distance_class_mul(kilometers, number, result):
+    distance = Distance(kilometers)
+    assert distance * number == result, (
+        f"Instance multiplied by {number} should equal to {result}, "
+        f"when instance.km equals to {kilometers}"
+    )
+
+
+@pytest.mark.parametrize(
+    'kilometers,number,result',
+    [
+        (50, 3, 16.67),
+        (30, 7, 4.29),
+        (45, 5, 9)
+    ]
+)
+def test_distance_class_truediv(kilometers, number, result):
+    distance = Distance(kilometers)
+    assert distance / number == result, (
+        f"Instance divided by {number} should equal to {result}, "
+        f"when instance.km equals to {kilometers}"
+    )
+
+
+@pytest.mark.parametrize(
+    'kilometers,number,result',
+    [
+        (50, 20, 10),
+        (30, 100, 30),
+        (45, 10, 5)
+    ]
+)
+def test_distance_class_mod(kilometers, number, result):
+    distance = Distance(kilometers)
+    assert distance % number == result, (
+        f"Instance modulo {number} should equal to {result}, "
+        f"when instance.km equals to {kilometers}"
+    )
+
+
+@pytest.mark.parametrize(
+    'kilometers,kilometers2,result',
+    [
+        (50, 50, True),
+        (100, 30, False),
+        (300, 100, False),
+        (30, 30, True)
+    ]
+)
+def test_distance_class_eq(kilometers, kilometers2, result):
+    distance = Distance(kilometers)
+    assert (distance == kilometers2) is result, (
+        f"'Distance({kilometers}) == {kilometers2}' should equal to {result}"
+    )
+
+
+@pytest.mark.parametrize(
+    'kilometers,kilometers2,result',
+    [
+        (50, 50, False),
+        (100, 30, True),
+        (300, 100, True),
+        (30, 100, False)
+    ]
+)
+def test_distance_class_gt(kilometers, kilometers2, result):
+    distance = Distance(kilometers)
+    assert (distance > kilometers2) is result, (
+        f"'Distance({kilometers}) > {kilometers2}' should equal to {result}"
+    )
+
+
+@pytest.mark.parametrize(
+    'kilometers,kilometers2,result',
+    [
+        (50, 50, True),
+        (100, 30, True),
+        (300, 100, True),
+        (30, 100, False)
+    ]
+)
+def test_distance_class_ge(kilometers, kilometers2, result):
+    distance = Distance(kilometers)
+    assert (distance >= kilometers2) is result, (
+        f"'Distance({kilometers}) => {kilometers2}' should equal to {result}"
+    )
+
+
+@pytest.mark.parametrize(
+    'kilometers,kilometers2,result',
+    [
+        (50, 50, False),
+        (100, 30, False),
+        (300, 100, False),
+        (30, 100, True)
+    ]
+)
+def test_distance_class_lt(kilometers, kilometers2, result):
+    distance = Distance(kilometers)
+    assert (distance < kilometers2) is result, (
+        f"'Distance({kilometers}) < {kilometers2}' should equal to {result}"
+    )
+
+
+@pytest.mark.parametrize(
+    'kilometers,kilometers2,result',
+    [
+        (50, 50, True),
+        (100, 30, False),
+        (300, 100, False),
+        (30, 100, True)
+    ]
+)
+def test_distance_class_le(kilometers, kilometers2, result):
+    distance = Distance(kilometers)
+    assert (distance <= kilometers2) is result, (
+        f"'Distance({kilometers}) <= {kilometers2}' should equal to {result}"
+    )
+
+
+@pytest.mark.parametrize(
+    'kilometers',
+    [
+        50,
+        100,
+        300
+    ]
+)
+def test_distance_class_len(kilometers):
+    distance = Distance(kilometers)
+    assert len(distance) == distance.km, (
+        f"'len()' for instance should return instance.km"
+    )
+
+
+
