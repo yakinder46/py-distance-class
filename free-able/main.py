bc5832bfe46243e5064b69704cbe511670a04604
Alex Nikitin <Nosify66@gmail.com>
2026-04-16 14:50:58 +0300

    'Solution'

--- free-able/main.py
+++ free-able/main.py
@@ -2,12 +2,8 @@ from typing import Any
 
 
 class Distance:
-    def __init__(self, metters: int | float, cm: int | float = 0) -> None:
-        self.meters_total = metters + cm / 100
-
-    @property
-    def km(self) -> float:
-        return self.meters_total
+    def __init__(self, km: int | float) -> None:
+        self.km = km
 
     def __str__(self) -> str:
         value = int(self.km) if self.km == int(self.km) else self.km
@@ -17,45 +13,35 @@ class Distance:
         value = int(self.km) if self.km == int(self.km) else self.km
         return f"Distance(km={value})"
 
-    def _get_m(self, other: Any) -> float:
+    def _get_km(self, other: Any) -> float:
         if isinstance(other, Distance):
-            return other.meters_total
+            return other.km
         return float(other)
 
     def __add__(self, other: Any) -> "Distance":
-        return Distance(self.meters_total + self._get_m(other))
+        return Distance(self.km + self._get_km(other))
 
     def __iadd__(self, other: Any) -> "Distance":
-        self.meters_total += self._get_m(other)
+        self.km += self._get_km(other)
         return self
 
     def __mul__(self, number: int | float) -> "Distance":
-        return Distance(self.meters_total * number)
+        return Distance(self.km * number)
 
     def __truediv__(self, number: int | float) -> "Distance":
-        return Distance(round(self.meters_total / number, 2))
+        return Distance(round(self.km / number, 2))
 
-    # Методы сравнения
     def __lt__(self, other: Any) -> bool:
-        return self.meters_total < self._get_m(other)
+        return self.km < self._get_km(other)
 
     def __gt__(self, other: Any) -> bool:
-        return self.meters_total > self._get_m(other)
+        return self.km > self._get_km(other)
 
     def __eq__(self, other: Any) -> bool:
-        return round(self.meters_total, 2) == round(self._get_m(other), 2)
+        return round(self.km, 2) == round(self._get_km(other), 2)
 
     def __le__(self, other: Any) -> bool:
-        return self.meters_total <= self._get_m(other)
+        return self.km <= self._get_km(other)
 
     def __ge__(self, other: Any) -> bool:
-        return self.meters_total >= self._get_m(other)
-
-
-class KiloDistance(Distance):
-    def __init__(self, km: int | float) -> None:
-        super().__init__(km * 1000)
-
-    @property
-    def km(self) -> float:
-        return self.meters_total / 1000
+        return self.km >= self._get_km(other)
